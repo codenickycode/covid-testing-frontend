bd983feac4f2c06d34ad8686b8908a713d8914c7
nfcarbone <nfcarbone@gmail.com>
2021-02-15 17:29:22 -0500

    reset password works

--- src/pages/Modals/Login/Login.js
+++ src/pages/Modals/Login/Login.js
@@ -1,19 +1,22 @@
 import React, { useState, useContext, useEffect } from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
 import tools from '../../../tools/index.js';
 import LoginForm from './Form.js';
 import useLogin from './useLogin.js';
 import { LoginSkeleton } from '../../../components/Skeletons.js';
-import { App } from '../../../Providers/Context.js';
+import { App, SetApp } from '../../../Providers/Context.js';
 
 const INIT = { email: '', password: '', confirmation: '' };
 
 const Login = ({ closeModal }) => {
   const { loading, error } = useContext(App);
+  const setApp = useContext(SetApp);
   const login = useLogin();
 
   const [signup, setSignup] = useState(true);
   const [forgot, setForgot] = useState(false);
+  const [passwordReset, setPasswordReset] = useState(false);
   const [inputs, setInputs] = useState(INIT);
   const [errors, setErrors] = useState(INIT);
 
@@ -35,15 +38,38 @@ const Login = ({ closeModal }) => {
   const handleSubmit = (e) => {
     e.preventDefault();
     if (!inputs.email) return setInvalid('email', 'Email required');
-    if (!tools.validPassword(inputs.password))
-      return setInvalid('password', 'Invalid password');
-    if (signup) {
-      if (inputs.password !== inputs.confirmation)
-        return setInvalid('confirmation', "Confirmation doesn't match");
+    if (forgot) {
       setErrors(INIT);
-      login('register', inputs.email, inputs.password);
+      resetPassword(inputs.email);
     } else {
-      login('login', inputs.email, inputs.password);
+      if (!tools.validPassword(inputs.password))
+        return setInvalid('password', 'Invalid password');
+      if (signup) {
+        if (inputs.password !== inputs.confirmation)
+          return setInvalid('confirmation', "Confirmation doesn't match");
+        setErrors(INIT);
+        login('register', inputs.email, inputs.password);
+      } else {
+        login('login', inputs.email, inputs.password);
+      }
+    }
+  };
+
+  const resetPassword = async (email) => {
+    setApp((prev) => ({ ...prev, loading: true }));
+    let confirmation = '',
+      error = '',
+      pwReset = false;
+    try {
+      const res = await axios.post('/common/forgot', { email });
+      confirmation = res.data;
+      pwReset = true;
+    } catch (e) {
+      error = e.response?.data || e.message;
+      console.log(error);
+    } finally {
+      setApp((prev) => ({ ...prev, loading: false, error, confirmation }));
+      setPasswordReset(pwReset);
     }
   };
 
@@ -67,6 +93,17 @@ const Login = ({ closeModal }) => {
             header={signup ? 'Registering...' : 'Logging In...'}
             message='Please wait while we complete your request.'
           />
+        ) : passwordReset ? (
+          <div id='password-reset'>
+            <h2>Password Reset</h2>
+            <p>A temporary password has been sent to your email.</p>
+            <p>
+              Click here to{' '}
+              <span className='click' onClick={closeModal}>
+                close this message
+              </span>
+            </p>
+          </div>
         ) : (
           <LoginForm
             signup={signup}
