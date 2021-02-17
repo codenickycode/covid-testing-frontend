48cbfce5efa05cb2cb26e2d03ba603b0c102a16b
nfcarbone <nfcarbone@gmail.com>
2021-02-17 14:51:00 -0500

    reset options and api path to use netlify proxy

--- src/api.js
+++ src/api.js
@@ -1,5 +1,6 @@
-export const api = 'https://covid-test-appointments.herokuapp.com/common';
+// export const api = 'https://covid-test-appointments.herokuapp.com/common';
+export const api = '/common';
 export const options = {
-  headers: { 'Content-Type': 'application/json' },
-  withCredentials: true,
+  //   headers: { 'Content-Type': 'application/json' },
+  //   withCredentials: true,
 };
